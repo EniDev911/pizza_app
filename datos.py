06732592b365c1a9840e3a56e92ba046bb0c52d7
enidev911 <enidev911@gmail.com>
2024-05-06 01:00:27 -0400

    segunda version: refactor

---
+++ datos.py
@@ -0,0 +1,67 @@
+ingredientes = {
+	'masa': 'Masa Tradicional',
+	'salsa': 'Salsa de Tomate',
+	'ingredientes': ['Queso']
+}
+
+T_MASA = '''Seleccione el tipo de Masa:
+
+(T) Masa Tradicional
+(D) Masa Delgada
+(B) Masa con Bordes de Queso
+
+Otra opción no modificará el tipo de Masa
+> '''
+
+T_SALSA = '''Seleccione el tipo de Salsa:
+
+(T) Salsa de Tomate
+(A) Salsa Alfredo
+(B) Salsa Berbecue
+(P) Salsa Pesto
+
+Otra opción no modificará el tipo de Salsa.
+> '''
+
+AG_INGREDIENTE = '''Seleccione el ingrediente que desea agregar:
+
+(1) Tomate
+(2) Champiñones
+(3) Aceituna
+(4) Cebolla
+(5) Pollo
+(6) Jamón
+(7) Carne
+(8) Tocino
+(9) Queso
+
+Otra opción no modificará los ingredientes actuales.
+> '''
+
+QT_INGREDIENTE = '''Seleccione el ingrediente que desea quitar:
+
+(1) Tomate
+(2) Champiñones
+(3) Aceituna
+(4) Cebolla
+(5) Pollo
+(6) Jamón
+(7) Carne
+(8) Tocino
+(9) Queso
+
+Otra opción no modificará los ingredientes actuales.
+> '''
+
+menu = '''¿Qué desea realizar?
+
+(1) Cambiar tipo de Masa
+(2) Cambiar tipo de Salsa
+(3) Agregar Ingredientes
+(4) Eliminar Ingredientes
+(5) Ordenar con los Ingredientes Actuales
+
+(0) Consultar ingredientes de la pizza
+
+Otro Número cancelará el pedido.
+> '''
